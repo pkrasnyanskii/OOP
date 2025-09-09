d572ec0d50dbf74e42f584fa65d5879d4a9b73d0
digitalunites <pr.krasnianski@gmail.com>
2025-09-09 15:40:47 +0700

    Javadocs for Demo.class

--- Task-1-1/src/main/java/ru/nsu/krasnyanski/Demo.java
+++ Task-1-1/src/main/java/ru/nsu/krasnyanski/Demo.java
@@ -2,11 +2,20 @@ package ru.nsu.krasnyanski;
 
 import java.util.Arrays;
 
+/**
+ * Demonstrates HeapSort with a sample array.
+ */
 public class Demo {
+
+    /**
+     * Main method to run the demonstration.
+     *
+     * @param args command-line arguments (not used)
+     */
     public static void main(String[] args) {
         int[] arr = {5, 3, 8, 1, 2, 7};
-        System.out.println("Before sorting: " + Arrays.toString(arr));
+        System.out.println("Before: " + Arrays.toString(arr));
         HeapSort.sort(arr);
-        System.out.println("After sorting: " + Arrays.toString(arr));
+        System.out.println("After: " + Arrays.toString(arr));
     }
 }
