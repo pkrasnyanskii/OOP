70c9d282a488d76c7a15d9e63b828e7d5d350ecf
digitalunites <pr.krasnianski@gmail.com>
2025-09-10 19:12:44 +0700

    Javadocs for Demo.class

--- Task-1-1/src/test/java/ru/nsu/krasnyanski/HeapSortTests.java
+++ Task-1-1/src/test/java/ru/nsu/krasnyanski/HeapSortTests.java
@@ -1,7 +1,7 @@
 package ru.nsu.krasnyanski;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
 
 public class HeapSortTests {
     @Test
