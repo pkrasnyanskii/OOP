f0e8d3226e6f6efa1395c384dab877e707308f99
digitalunites <pr.krasnianski@gmail.com>
2025-09-10 12:24:48 +0700

    Fixed mistakes + Demo class for shell script demonstration

---
+++ Task-1-1/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# exit при любой ошибке
+set -e
+
+# исходники
+SRC_DIR=./src/main/java
+# папка для скомпилированных классов
+BIN_DIR=./out
+# папка для документации
+DOC_DIR=./docs
+# имя jar
+JAR_FILE=HeapSort.jar
+
+# создаём папки
+mkdir -p $BIN_DIR
+mkdir -p $DOC_DIR
+
+# компиляция всех Java-файлов
+javac -d $BIN_DIR $(find $SRC_DIR -name "*.java")
+
+# генерация Javadoc
+javadoc -d $DOC_DIR $(find $SRC_DIR -name "*.java")
+
+# упаковка в jar
+cd $BIN_DIR
+jar cfe $JAR_FILE ru.nsu.krasnyanski.HeapSort ru/nsu/krasnyanski/*.class
+cd ..
+
+# запуск приложения
+java -cp $BIN_DIR/$JAR_FILE ru.nsu.krasnyanski.Demo
