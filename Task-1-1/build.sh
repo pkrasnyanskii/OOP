b14531ee14df4fdb7e903b9c8837e52dabb2f6c0
digitalunites <pr.krasnianski@gmail.com>
2025-09-13 15:12:04 +0700

    Добавил сборку и запуск JAR-файла

--- Task-1-1/build.sh
+++ Task-1-1/build.sh
@@ -5,10 +5,13 @@ rm -rf out docs
 mkdir out docs
 
 # Компиляция
-javac -d out src/main/java/ru/nsu/krasnyanski/HeapSort.java src/main/java/ru/nsu/krasnyanski/HeapSortDemo.java
+javac -d out src/main/java/ru/nsu/krasnyanski/*.java
 
 # Документация
 javadoc -d docs -sourcepath src/main/java -subpackages ru.nsu.krasnyanski
 
-# Запуск демо
-java -cp out ru.nsu.krasnyanski.HeapSortDemo
+# Создание JAR
+jar cfe out/HeapSortDemo.jar ru.nsu.krasnyanski.HeapSortDemo -C out .
+
+# Запуск JAR
+java -jar out/HeapSortDemo.jar
