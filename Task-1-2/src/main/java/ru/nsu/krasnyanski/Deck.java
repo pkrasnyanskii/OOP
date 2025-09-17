6b2b4594c84fa5ee4b34f1afa4a3bf7406ead49c
digitalunites <pr.krasnianski@gmail.com>
2025-09-17 10:05:30 +0700

    Soft deadline draft

--- Task-1-2/src/main/java/ru/nsu/krasnyanski/Deck.java
+++ Task-1-2/src/main/java/ru/nsu/krasnyanski/Deck.java
@@ -8,10 +8,7 @@ import java.util.*;
 public class Deck {
     private final List<Card> cards = new ArrayList<>();
 
-    public Deck() {
-        // пока можно оставить заглушку
-        // позже сюда добавим генерацию всех 52 карт
-    }
+    public Deck() {}
 
     public Card draw() {
         return cards.remove(0);
