abd20864c3df99510d26edf7ad1ff43313d61f6b
digitalunites <pr.krasnianski@gmail.com>
2025-12-25 16:00:00 +0700

    CL third try

--- Task-1-5-1/src/test/java/ru/nsu/krasnyanski/markdown/structure/CodeBlockTest.java
+++ Task-1-5-1/src/test/java/ru/nsu/krasnyanski/markdown/structure/CodeBlockTest.java
@@ -2,7 +2,6 @@ package ru.nsu.krasnyanski.markdown.structure;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
 import org.junit.jupiter.api.Test;
 
 class CodeBlockTest {
