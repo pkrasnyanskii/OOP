b4e259d34739c466b4b4c3075d6fe94f81f59c3d
digitalunites <pr.krasnianski@gmail.com>
2026-03-27 14:18:10 +0700

    Remove extra separation in imports

--- Task-2-2-1/src/test/java/ru/nsu/krasnyanskii/pizzeria/OrderSerializerTest.java
+++ Task-2-2-1/src/test/java/ru/nsu/krasnyanskii/pizzeria/OrderSerializerTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
